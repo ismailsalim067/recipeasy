2f45317e14c3ffb651b980d3a137e265ead8c40c
“Adam <2915142d@student.gla.ac.uk>
2026-03-06 19:47:37 +0000

    Add homepage view and URL routing

---
+++ recipes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'recipes'
+
+urlpatterns = [
+    path("", views.redirect_to_homepage, name="home"),
+    path("homepage/", views.home, name="homepage"),
+]
