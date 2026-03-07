f6def243b02ef28c624624dbe78159376bb81fb6
Adam D <65791418+qrxxy@users.noreply.github.com>
2026-03-07 13:26:53 +0000

    Add homepage view and URL routing
    Co-authored-by: “Adam <“2915142d@student.gla.ac.uk”>

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    #return render(request)
+    return HttpResponse("Recipes Home page")
+
+def redirect_to_homepage(request):
+    return redirect("homepage/")
+
+
