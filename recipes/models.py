f231d57fc3629663cd4fbd1147a66bfa129f1d6a
Adam D <65791418+qrxxy@users.noreply.github.com>
2026-03-11 23:46:53 +0000

    Create recipe initial model
    * Create Recipes template view
    
    * starter create recipe model
    
    ---------
    
    Co-authored-by: “Adam <“2915142d@student.gla.ac.uk”>

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+CUISINE_CHOICES = [
+    ('italian', 'Italian'),
+    ('indian', 'Indian'),
+    ('mexican', 'Mexican'),
+    ('chinese', 'Chinese'),
+    ('other', 'Other'),
+]
+
+DIFFICULTY_CHOICES = [
+    ('easy' , 'Easy'),
+    ('medium', 'Medium'),
+    ('hard', 'hard'),
+]
+
+class Recipes(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    cuisine = models.CharField(max_length=50, choices=CUISINE_CHOICES)
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+    cooking_time = models.PositiveIntegerField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.name
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Recipes
+
+
 
 # Create your views here.
 
@@ -12,6 +15,34 @@ def redirect_to_homepage(request):
 
 def create_recipe(request):
     return HttpResponse("Create Recipe view")
+
+
+
+
+def create_recipe(request):
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        cuisine = request.POST.get("cuisine")
+        difficulty = request.POST.get("difficulty")
+        cooking_time = request.POST.get("cooking_time")
+        ingredients = request.POST.get("ingredients")
+        instructions = request.POST.get("instructions")
+
+        recipe = Recipes.objects.create(
+            name=name,
+            description=description,
+            cuisine=cuisine,
+            difficulty=difficulty,
+            cooking_time=cooking_time,
+            ingredients=ingredients,
+            instructions=instructions
+        )
+
+        return HttpResponse(f"Recipe {recipe.name} created")
+     
+    return HttpResponse('Waiting for recipe submission')
 
 
 
