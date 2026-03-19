1617bd3b0ad09221ed1bf98bd490a4776e78d9d9
Ismail Salim <3011687S@student.gla.ac.uk>
2026-03-19 17:00:08 +0000

    Add auth and create recipe tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -56,13 +56,67 @@ class AuthViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertTrue(User.objects.filter(username="newuser").exists())
 
+    def test_signup_redirects_to_homepage(self):
+        response = self.client.post(
+            reverse("recipes:signup"),
+            {
+                "username": "redirectuser",
+                "password1": "StrongPass123!",
+                "password2": "StrongPass123!",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/homepage/")
+
+    def test_signup_logs_user_in(self):
+        self.client.post(
+            reverse("recipes:signup"),
+            {
+                "username": "autologinuser",
+                "password1": "StrongPass123!",
+                "password2": "StrongPass123!",
+            },
+        )
+
+        response = self.client.get(reverse("recipes:myrecipes"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_with_valid_credentials_redirects_to_homepage(self):
+        User.objects.create_user(username="loginuser", password="StrongPass123!")
+
+        response = self.client.post(
+            reverse("recipes:login"),
+            {
+                "username": "loginuser",
+                "password": "StrongPass123!",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/homepage/")
+
+    def test_login_with_invalid_credentials_shows_error(self):
+        User.objects.create_user(username="wrongpassuser", password="StrongPass123!")
+
+        response = self.client.post(
+            reverse("recipes:login"),
+            {
+                "username": "wrongpassuser",
+                "password": "WrongPass123!",
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Invalid username or password.")
+
     def test_logged_out_user_is_redirected_from_my_recipes(self):
         response = self.client.get(reverse("recipes:myrecipes"))
         self.assertEqual(response.status_code, 302)
         self.assertIn(reverse("recipes:login"), response.url)
 
     def test_logged_in_user_can_access_my_recipes(self):
-        user = User.objects.create_user(username="testuser", password="StrongPass123!")
+        User.objects.create_user(username="testuser", password="StrongPass123!")
         self.client.login(username="testuser", password="StrongPass123!")
 
         response = self.client.get(reverse("recipes:myrecipes"))
@@ -70,8 +124,36 @@ class AuthViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "You have 0 recipe(s).")
 
+    def test_logged_out_user_is_redirected_from_create_recipe(self):
+        response = self.client.get(reverse("recipes:createrecipe"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse("recipes:login"), response.url)
+
+    def test_logged_in_user_can_create_recipe_with_author(self):
+        user = User.objects.create_user(username="recipeuser", password="StrongPass123!")
+        self.client.login(username="recipeuser", password="StrongPass123!")
+
+        response = self.client.post(
+            reverse("recipes:createrecipe"),
+            {
+                "name": "Soup",
+                "description": "Warm soup",
+                "cuisine": "other",
+                "difficulty": "easy",
+                "cooking_time": 15,
+                "ingredients": "Water, vegetables",
+                "instructions": "Boil and serve",
+            },
+        )
+
+        self.assertEqual(response.status_code, 302)
+        recipe = Recipes.objects.get(name="Soup")
+        self.assertEqual(recipe.author, user)
+        self.assertEqual(response.url, reverse("recipes:recipe_detail", args=[recipe.id]))
+
     def test_logout_redirects_to_homepage(self):
-        user = User.objects.create_user(username="logoutuser", password="StrongPass123!")
+        User.objects.create_user(username="logoutuser", password="StrongPass123!")
         self.client.login(username="logoutuser", password="StrongPass123!")
 
         response = self.client.get(reverse("recipes:logout"))
