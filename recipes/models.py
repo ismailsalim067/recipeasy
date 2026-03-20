a4752f8df11386baed9e2c407b06fd230ffbb72e
Ismail Salim <3011687S@student.gla.ac.uk>
2026-03-20 17:44:02 +0000

    Add rating model for individual recipes

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -30,3 +31,17 @@ class Recipes(models.Model):
 
     def __str__(self):
         return self.name
+
+class Rating(models.Model):
+    recipe = models.ForeignKey(Recipes, on_delete=models.CASCADE, related_name='ratings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+    value = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['recipe', 'user'], name='unique_user_recipe_rating')
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} rated {self.recipe.name}: {self.value}"
